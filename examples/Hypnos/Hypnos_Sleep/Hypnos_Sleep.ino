ddc5a457d8bcd1e65147b3a9bb19551d1ee795e1
Rowan Bignell <rowanbignell@gmail.com>
2026-09-14 12:57:44 -0700

    bring over example hypnos_sleep.ino

---
+++ examples/Hypnos/Hypnos_Sleep/Hypnos_Sleep.ino
@@ -0,0 +1,46 @@
+/**
+ * This is an example use case for the Hypnos board's sleep functionality
+ * This allows the user to put the Feather into a deep sleep disabling power to all sensors and then resume operation after a given length of time
+ * 
+ * MANAGER MUST BE INCLUDED FIRST IN ALL CODE
+ */
+
+#include <Loom_Manager.h>
+
+#include <Hardware/Loom_Hypnos/Loom_Hypnos.h>
+
+// Manager to control the device
+Manager manager("Device", 1);
+
+// Create a new Hypnos object setting the version to determine the SD Chip select pin, and starting without the SD card functionality
+//Loom_Hypnos(Manager& man, HYPNOS_VERSION version, TIME_ZONE zone, bool use_custom_time = false, bool useSD = true)
+Loom_Hypnos hypnos(manager, HYPNOS_VERSION::V3_3, TIME_ZONE::PST, false, false);
+
+// Called when the interrupt is triggered 
+void isrTrigger(){
+  hypnos.wakeup();
+}
+
+void setup() {
+
+  // Start the serial interface
+  manager.beginSerial();
+
+  // Enable the hypnos rails
+  hypnos.enable();
+  
+  // Register the ISR and attach to the interrupt
+  hypnos.registerInterrupt(isrTrigger);
+}
+
+void loop() {
+
+  // Set the RTC interrupt alarm to wake the device in 10 seconds
+  hypnos.setInterruptDuration(TimeSpan(0, 0, 0, 10));
+
+  // Reattach to the interrupt after we have set the alarm so we can have repeat triggers
+  hypnos.reattachRTCInterrupt();
+  
+  // Put the device into a deep sleep, operation HALTS here until the interrupt is triggered
+  hypnos.sleep();
+}
