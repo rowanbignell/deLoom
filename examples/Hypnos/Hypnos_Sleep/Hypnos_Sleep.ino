c035220ed05c6191a8f9ebfa5935c208f61853c3
Rowan Bignell <rowanbignell@gmail.com>
2026-09-15 13:12:06 -0700

    add measure package etc to ino

--- examples/Hypnos/Hypnos_Sleep/Hypnos_Sleep.ino
+++ examples/Hypnos/Hypnos_Sleep/Hypnos_Sleep.ino
@@ -14,7 +14,7 @@ Manager manager("Device", 1);
 
 // Create a new Hypnos object setting the version to determine the SD Chip select pin, and starting without the SD card functionality
 //Loom_Hypnos(Manager& man, HYPNOS_VERSION version, TIME_ZONE zone, bool use_custom_time = false, bool useSD = true)
-Loom_Hypnos hypnos(manager, HYPNOS_VERSION::V3_3, TIME_ZONE::PST, false, false);
+Loom_Hypnos hypnos(manager, false);
 
 // Called when the interrupt is triggered 
 void isrTrigger(){
@@ -28,6 +28,8 @@ void setup() {
 
   // Enable the hypnos rails
   hypnos.enable();
+
+  manager.initialize();
   
   // Register the ISR and attach to the interrupt
   hypnos.registerInterrupt(isrTrigger);
@@ -37,6 +39,12 @@ void loop() {
 
   // Set the RTC interrupt alarm to wake the device in 10 seconds
   hypnos.setInterruptDuration(TimeSpan(0, 0, 0, 10));
+
+  manager.measure();
+
+  manager.package();
+
+  manager.display();
 
   // Reattach to the interrupt after we have set the alarm so we can have repeat triggers
   hypnos.reattachRTCInterrupt();
